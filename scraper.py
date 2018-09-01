1efffd7112a032fd7e1304e84e48b08c5fe9c0fc
LAPTOP-VH1O0917\Lucas <lucascoats663@gmail.com>
2018-09-01 17:45:00 -0400

    Cleaned to PEP

--- scraper.py
+++ scraper.py
@@ -1,39 +1,40 @@
 import bs4
-from urllib.request import urlopen as uReq
+from urllib.request import urlopen
 from bs4 import BeautifulSoup as soup
 
 # Get first url data
-my_url = "https://www.newegg.com/Desktop-Graphics-Cards/SubCategory/ID-48?Tid=7709&cm_sp=Tab_Gaming-Video-Cards_1-_-LeftNav-_-All-Cards"
-uClient = uReq(my_url)
-page_html = uClient.read()
-uClient.close()
+input_url = input("Enter full newegg paged url: ")
+my_url = "https://www.newegg.com/Gaming-Desktops/SubCategory/ID-3742?Tid=897483"
+u_client = urlopen(my_url)
+page_html = u_client.read()
+u_client.close()
 page_soup = soup(page_html, "html.parser")
 
 # Set max pages
-nav = page_soup.find_all("div", {"id": "page_NavigationBar"})
-btns = nav[-1].find_all("div", {"class": "btn-group-cell"})
+nav_bar = page_soup.find_all("div", {"id": "page_NavigationBar"})
+btns = nav_bar[-1].find_all("div", {"class": "btn-group-cell"})
 max_pages = int(btns[9].button.text)
 
 # Create csv file
-filename = "products.csv"
+filename = "products1.csv"
 f = open(filename, "w")
 headers = "brand, product_name, shipping\n"
 f.write(headers)
 
 # Loop through url pages and get product containers
 for page in range(1, 54):
-    paged_url = f"https://www.newegg.com/Desktop-Graphics-Cards/SubCategory/ID-48/Page-{page}?Tid=7709&cm_sp=Tab_Gaming-Video-Cards_1-_-LeftNav-_-All-Cards"
-    uClient = uReq(my_url)
-    page_html = uClient.read()
-    uClient.close()
+    paged_url = f"https://www.newegg.com/Gaming-Desktops/SubCategory/ID-3742/Page-{page}?Tid=897483"
+    uClient = urlopen(my_url)
+    page_html = u_client.read()
+    u_client.close()
     page_soup = soup(page_html, "html.parser")
-    containers = page_soup.findAll("div", {"class": "item-container"})
+    containers = page_soup.find_all("div", {"class": "item-container"})
 
 # Loop through product containers
     for container in containers:
         brand = container.div.div.img["title"]
         product_name = container.a.img["alt"]
-        shipping_container = container.findAll("li", {"class": "price-ship"})
+        shipping_container = container.find_all("li", {"class": "price-ship"})
         shipping = shipping_container[0].text.strip()
         f.write(f"{brand.replace(',', '|')},{product_name.replace(',', '|')},{shipping.replace(',', '|')}\n")
 
