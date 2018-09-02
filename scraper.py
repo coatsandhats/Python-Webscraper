2f99015ebac498fd7f1040c0bb0d0210cc4b1923
LAPTOP-VH1O0917\Lucas <lucascoats663@gmail.com>
2018-09-02 12:14:38 -0400

    Added price scraping, None and free quick view

--- scraper.py
+++ scraper.py
@@ -17,7 +17,7 @@ max_pages = int(btns[-2].button.text)
 # Create csv file
 filename = input("Enter csv file name(no extension): ") + ".csv"
 f = open(filename, "w")
-headers = "brand, product_name, shipping\n"
+headers = "brand, product_name, price, shipping\n"
 f.write(headers)
 
 # Get and check page range
@@ -35,9 +35,9 @@ for page in range(start, end):
     if my_url.split("?")[0][-7] == "/":
         split_url = [my_url.split("?")[0].split("/P")[0], my_url.split("?")[1]]
         paged_url = f"{split_url[0]}/Page-{page}?{split_url[1]}"
-    elif "IsNodeId=1&" in my_url:
+    elif "IsNodeId=1&N" in my_url:
         paged_url = f"{my_url}&Page={page}"
-    elif "IsNodeId=1&bop" in my_url:
+    elif "IsNodeId=1&bop" or "IsNodeId=1%p" in my_url:
         split_url = [my_url.split("PageSize")[0].split("Page=")[0] + "Page=", "PageSize" + my_url.split("PageSize")[1]]
         paged_url = f"{split_url[0]}{page}{split_url[1]}"
     else:
@@ -54,9 +54,15 @@ for page in range(start, end):
         brand = container.div.div.img["title"]
         product_name = container.a.img["alt"]
         shipping_container = container.find_all("li", {"class": "price-ship"})
-        shipping = shipping_container[0].text.strip()
+        shipping = shipping_container[0].text.strip().split("Ship")[0]
+
+        price_container = container.find_all("li", {"class": "price-current"})
+        if price_container[0].strong is None:
+            price = "None"
+        else:
+            price = price_container[0].strong.text
         # fix for csv github quotes
         format_product_name = (product_name.replace(',', '|')).replace('"', "inch")
-        f.write(f"{brand.replace(',', '|')},{format_product_name},{shipping.replace(',', '|')}\n")
+        f.write(f"{brand.replace(',', '|')},{format_product_name}, {price.replace(',', '')}, {shipping.replace(',', '')}\n")
 
 f.close()
